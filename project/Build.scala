1893f3c89cb2068420d8b364cf798b4cc7e6c77e
Marc Esquerrà i Bayo <esquerra@w3d.cc>
2013-08-18 18:54:13 +0100

    Fixing publish issues

--- project/Build.scala
+++ project/Build.scala
@@ -6,10 +6,36 @@ object BuildSettings
 
 	val buildSettings = Defaults.defaultSettings ++ Seq(
 			organization := "com.bryghts.kissnumber",
-			version      := "0.0.1-SNAPSHOT",
+			version      := "0.0.1",
 			scalaVersion := "2.10.1",
-			scalacOptions ++= Seq() //,
-			//libraryDependencies += "com.amazonaws" % "aws-java-sdk" % "1.4.7"
+			scalacOptions ++= Seq(),
+			publishMavenStyle := true,
+			publishTo <<= version { (v: String) =>
+				val nexus = "https://oss.sonatype.org/"
+				if (v.trim.endsWith("SNAPSHOT")) Some("snapshots" at nexus + "content/repositories/snapshots") 
+				else                             Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+			},
+			publishArtifact in Test := false,
+//			useGpg := true,
+			pomExtra := (
+				<url>http://www.brights.com</url>
+				<licenses>
+					<license>
+					<name>mit</name>
+					</license>
+				</licenses>
+				<scm>
+					<url>git@github.com:marcesquerra/KissNumber.git</url>
+					<connection>scm:git:git@github.com:marcesquerra/KissNumber.git</connection>
+				</scm>
+				<developers>
+					<developer>
+						<name>Marc Esquerrà i Bayo</name>
+						<email>esquerra@bryghts.com</email>
+					</developer>
+				</developers>
+			),
+			pomIncludeRepository := { _ => false }
 	)
 
 }
